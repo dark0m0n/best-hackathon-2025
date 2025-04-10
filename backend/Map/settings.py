36cc13c84ac906bef361b22b29ec5cdb0e3c410b
xed <ana1lphaon@gmail.com>
2025-04-10 22:26:10 +0300

    start

--- backend/Map/settings.py
+++ backend/Map/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',  # React default port
+    os.environ["CORS_ALLOWED_ORIGINS"],
 ]
