da12c6f110ec855c573b54d66fdbc82d3e768261
xed <ana1lphaon@gmail.com>
2025-04-11 23:04:13 +0300

    added use of gis

--- backend/Map/settings.py
+++ backend/Map/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     'rest_framework',
     'api',
     'corsheaders',
@@ -79,8 +80,8 @@ WSGI_APPLICATION = 'Map.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': BASE_DIR / 'db',
     }
 }
 
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,4 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
 # Create your models here.
