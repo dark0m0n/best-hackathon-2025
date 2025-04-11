869ee026d9ed9593e2c895bb2c724fcd0037f2d9
xed <ana1lphaon@gmail.com>
2025-04-12 02:04:43 +0300

    created Location, Review

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,16 @@
-from django.db import models
-from django.contrib.gis.db import models as gis_models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    coordinates = models.PointField()
+    has_ramp = models.BooleanField(default=False)
+    has_tactile_elements = models.BooleanField(default=False)
+    has_adapted_toilet = models.BooleanField(default=False)
+    category = models.CharField(max_length=50)
+
+class Review(models.Model):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
