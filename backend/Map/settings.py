5ded5d4088bc4db7d3aaccdee3037b99ca349493
xed <ana1lphaon@gmail.com>
2025-04-12 02:03:14 +0300

    added load_dotenv and fixed issue with db

--- backend/Map/settings.py
+++ backend/Map/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +84,8 @@ WSGI_APPLICATION = 'Map.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': BASE_DIR / 'db',
+        'NAME': 'db',
+        'USER': os.environ["DB_USER"],
     }
 }
 
