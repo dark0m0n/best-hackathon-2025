b4afa9cb33ab949b55c6c41a51bb99bc3c23c032
XxKotfeJxX <povelitel.bog.lol@gmail.com>
2025-04-13 14:41:56 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MapPage from './pages/MapPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 import LocationDetails from './pages/LocationDetails'; // 🆕
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/map" element={<MapPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/location/:id" element={<LocationDetails />} /> {/* 🆕 */}
       </Routes>
     </Router>
